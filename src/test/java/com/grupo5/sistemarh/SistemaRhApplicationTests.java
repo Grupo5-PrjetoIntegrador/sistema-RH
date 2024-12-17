c55273bea7ce8019f68b902a1a08e28db68b5262
CaciaRodrigues <caciarodriguues@gmail.com>
2024-12-17 10:41:49 -0300

    Preparando o Ambiente

---
+++ src/test/java/com/grupo5/sistemarh/SistemaRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.sistemarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
